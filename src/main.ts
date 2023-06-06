7abb3c6eb94530d22db74b0134b98486cf831235
seshankavisanka <seshankavisanka@gmail.com>
2023-06-06 15:20:59 +0530

    initialize Swagger using the SwaggerModule class

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('HS code finder')
+    .setDescription('The HS code finder API description')
+    .setVersion('1.0')
+    .addTag('HScode')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }));
